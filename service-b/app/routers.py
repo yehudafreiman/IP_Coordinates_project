7fef720d8d99dcf26b365495603ecd9d4b33598d
Yehuda Freiman <yehfre@icloud.com>
2026-01-13 15:14:11 +0200

    add commits

--- service-b/app/routers.py
+++ service-b/app/routers.py
@@ -3,12 +3,16 @@ import requests
 from storage import redis_store, redis_retrieve
 from schemas import CoordinatesWithIP
 
+# פונקציית עזר
 def load_external_data(url):
+    # שלוף נתונים מ-url
     response = requests.get(url)
+    # החזר בפורמט json
     return response.json()
 
 router = APIRouter()
 
+# נקודת קצה לקבלת נתונים ושמירה במסד נתונים
 @router.post("/saveCoordinatesToRedis")
 def save_coordinates_to_redis():
     # שלוף נתונים מ-Service A
@@ -20,9 +24,13 @@ def save_coordinates_to_redis():
     # החזר תשובה למשתמש
     return {"message": "Saved successfully", "ip": validated.ip}
 
+# נקודת קצה לקבלת נתונים ממסד נתונים והחזרה למשתמש
 @router.get("/getCoordinates/{ip}", response_model=CoordinatesWithIP)
 def get_coordinates(ip: str):
+    # שלוף נתונים ממסד נתונים
     data = redis_retrieve(ip)
+    # אם התקבלו נתונים החזר אותם למשתמש
     if data:
         return data
+    # אם לא התקבלו נתונים החזר שגיאה
     raise HTTPException(status_code=404, detail="IP not found")
