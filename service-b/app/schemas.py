dd491bcc54ce808c53833ea5b066aa960f156e33
Yehuda Freiman <yehfre@icloud.com>
2026-01-13 14:46:09 +0200

    implement validation

--- service-b/app/schemas.py
+++ service-b/app/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, Field
 
-
 class Coordinates(BaseModel):
     lat: float = Field(..., ge=-90, le=90, description="Latitude")
     lon: float = Field(..., ge=-180, le=180, description="Longitude")
+
+class CoordinatesWithIP(Coordinates):
+    ip: str
