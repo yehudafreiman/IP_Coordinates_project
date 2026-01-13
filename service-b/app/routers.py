8e340af8b209955e903596955c7968962f6d56b7
Yehuda Freiman <yehfre@icloud.com>
2026-01-13 15:55:46 +0200

    add try-except to @router.post(/saveCoordinatesToRedis)

--- service-b/app/routers.py
+++ service-b/app/routers.py
@@ -15,22 +15,31 @@ router = APIRouter()
 # נקודת קצה לקבלת נתונים ושמירה במסד נתונים
 @router.post("/saveCoordinatesToRedis")
 def save_coordinates_to_redis():
-    # שלוף נתונים מ-Service A
-    data = load_external_data("http://localhost:8000/coordinates")
-    # בדוק שהנתונים תקינים
-    validated = CoordinatesWithIP(**data)
-    # שמור ב-Redis
-    redis_store(validated.ip, validated.dict())
-    # החזר תשובה למשתמש
-    return {"message": "Saved successfully", "ip": validated.ip}
+    try:
+        # שלוף נתונים מ-Service A
+        data = load_external_data("http://localhost:8000/coordinates")
+        # בדוק שהנתונים תקינים
+        validated = CoordinatesWithIP(**data)
+        # שמור ב-Redis
+        redis_store(validated.ip, validated.model_dump())
+        # החזר תשובה למשתמש
+        return {
+            "message": "Saved successfully",
+            "ip": validated.ip
+        }
+    # אם לא נתונים לא תקינים או שקיימת שגיאת חיבור – החזר שגיאה
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to save coordinates")
 
 # נקודת קצה לקבלת נתונים ממסד נתונים והחזרה למשתמש
 @router.get("/getCoordinates/{ip}", response_model=CoordinatesWithIP)
 def get_coordinates(ip: str):
-    # שלוף נתונים ממסד נתונים
+    # שלוף נתונים ממסד נתונים לפי ה-IP
     data = redis_retrieve(ip)
-    # אם התקבלו נתונים החזר אותם למשתמש
-    if data:
+    # אם נמצא ערך ב-Redis
+    if data is not None:
         return data
-    # אם לא התקבלו נתונים החזר שגיאה
+    # אם לא נמצא ערך – החזר שגיאה
     raise HTTPException(status_code=404, detail="IP not found")
