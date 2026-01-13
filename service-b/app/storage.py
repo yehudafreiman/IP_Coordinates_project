d46ebdda3f3240396ce4387b541a5842a02b8d21
Yehuda Freiman <yehfre@icloud.com>
2026-01-13 14:39:23 +0200

    implement connection to redis

--- service-b/app/storage.py
+++ service-b/app/storage.py
@@ -1,15 +1,18 @@
 from redis import Redis
+import json
 
 redis_conn = Redis(host="localhost", port=6379, decode_responses=True)
 
 def redis_store(key, value):
+    if isinstance(value, dict):
+        value = json.dumps(value)
     redis_conn.set(key, value)
 
 def redis_retrieve(key):
-    return redis_conn.get(key)
-
-redis_store('foo', 'bar')
-retrieved = redis_retrieve('foo')
-print(retrieved)
-
-redis_conn.close()
+    value = redis_conn.get(key)
+    if value:
+        try:
+            return json.loads(value)
+        except json.JSONDecodeError:
+            return value
+    return None
